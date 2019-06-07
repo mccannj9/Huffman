6e62aceaf029e0a0a6e64bd18c61006ca61a2186
Jamie <jamcc09@gmail.com>
2019-06-07 18:12:56 +0200

    Adding methods to update node parents

--- Tree.cc
+++ Tree.cc
@@ -9,12 +9,23 @@ struct Node {
   Node* parent;
 
   Node();
-  Node(std::string value, double freq) : value(value), freq(freq){}
-  Node(Node* left_child, Node* right_child); //, std::string character, double frequency);
 
+  Node(std::string value, double freq) : value(value), freq(freq) {
+    left = nullptr;
+    right = nullptr;
+    parent = nullptr;
+  }
+
+  Node(Node* left_child, Node* right_child);
+
+  void update_parent(Node* new_parent);
   bool is_leaf();
   bool is_root();
 };
+
+void Node::update_parent(Node* new_parent) {
+  parent = new_parent;
+}
 
 Node::Node() {
   value = 'x';
@@ -25,7 +36,7 @@ Node::Node() {
 }
 
 
-Node::Node(Node* left_child, Node* right_child) {//, std::string s, double f) {
+Node::Node(Node* left_child, Node* right_child) {
   left = left_child;
   right = right_child;
   value = left->value + "_" + right->value;
@@ -61,8 +72,15 @@ int main() {
   Node child_left = Node("c", 0.1);
   Node child_right = Node("a", 0.2);
   Node root = Node(&child_left, &child_right);
+  root.left->update_parent(&root);
+  root.right->update_parent(&root);
+
   std::printf("Root value %s, Root freq %f\n", root.value.c_str(), root.freq);
   std::printf("Root is leaf? %d\n", (int)root.is_leaf());
   std::printf("Root is root? %d\n", (int)root.is_root());
+  std::printf("Left is leaf? %d\n", (int)root.left->is_leaf());
+  std::printf("Right is leaf? %d\n", (int)root.right->is_leaf());
+  std::printf("Left is root? %d\n", (int)root.left->is_root());
+  std::printf("Right is root? %d\n", (int)root.right->is_root());
   return 0;
 }
